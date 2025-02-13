1c9344c6601a22f46a3cf8bbc0e6259da8b4dc54
TimothyCMeehan <tcmeehan@gmail.com>
2025-02-13 08:01:43 -0500

    add graceful shutdown

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,46 @@ import app from "./app";
 import config from "./config/EnvConfig"
 import logger from './utils/logger';
 
-app.listen(config.PORT, () => {
-    logger.info(`🚀 Server is running on PORT ${config.PORT} in the ${config.APP_ENV} environment`);
-});
+let server: any;
+
+async function startServer() {
+    try {
+
+        server = app.listen(config.PORT, () => {
+            logger.info(`🚀 Server is running on PORT ${config.PORT} in the ${config.APP_ENV} environment`);
+        });
+
+        // Handle graceful shutdown
+        process.on('SIGINT', stopServer);
+        process.on('SIGTERM', stopServer);
+
+    } catch (error) {
+        logger.error('❌ Failed to start the server:', error);
+        process.exit(1);
+    }
+}
+
+async function stopServer() {
+    logger.info('⚠️ Shutting down server...');
+
+    try {
+        if (server) {
+            server.close(() => {
+                logger.info('✅ HTTP server closed.');
+                process.exit(0);
+            });
+        } else {
+            process.exit(0);
+        }
+
+        
+
+    } catch (error) {
+
+        logger.error('❌ Error during shutdown:', error);
+        process.exit(1);
+
+    }
+}
+
+startServer();
